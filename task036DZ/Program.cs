d52ac5c8944f895eb6344e2dfc0b5c49ca3d3475
paha85 <padisb@gmail.com>
2023-10-11 11:07:14 +0300

    DZ-5

---
+++ task036DZ/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте одномерный массив, заполненный
+// случайными числами. Найдите сумму элементов, стоящих
+// на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+        
+    }
+}
+
+Console.Write("Pls enter number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+FillArray(arr);
+Console.WriteLine($"Массив:\n{string.Join("\t", arr)}");
+
+int sumOfNumbers = 0;
+for (int i = 1; i < arr.Length; i+=2)
+{
+    sumOfNumbers += arr[i];    
+}
+
+Console.WriteLine($"Сумма элементов нечетных индексов: {sumOfNumbers}");
---
+++ task038DZ/Program.cs
@@ -0,0 +1,40 @@
+﻿//  Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным
+// элементов массива.
+// [3 7 22 2 78] -> 76
+
+void FillArray(int[] arr)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(0, 100);
+        
+    }
+}
+
+Console.Write("Pls enter number of elements: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[size];
+
+FillArray(arr);
+Console.WriteLine($"Массив:\n{string.Join("\t", arr)}");
+
+int max = arr[0];
+int min = arr[0];
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+    {
+        max = arr[i];
+    }
+    else if (arr[i] < min)
+    {
+        min = arr[i];
+    }
+}
+
+Console.WriteLine($"max number in aray: {max}");
+Console.WriteLine($"min number in aray: {min}");
+Console.WriteLine($"diff between max and min: {max}-{min}={max-min}");
